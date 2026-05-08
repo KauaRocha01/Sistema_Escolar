a3605e327806be7a4a73e328e7c24ca83738a2db
KauaRocha01 <kauar6490@gmail.comgit config --global user.email kauar6490@gmail.com>
2026-05-07 21:54:11 -0300

    Sistema Escolar

--- untitled/src/main/java/daolmplements/AlunoDAOImplements.java
+++ untitled/src/main/java/daolmplements/AlunoDAOImplements.java
@@ -1,19 +1,57 @@
 package daolmplements;
 
+import database.slqConn;
 import dao.IAlunoDAO;
 import model.Aluno;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class AlunoDAOImplements implements IAlunoDAO {
     @Override
     public void salvarAluno(Aluno aluno) {
 
+
+
+
+
     }
 
     @Override
-    public List<Aluno> listarTodosAlunos() {
-        return List.of();
+    public List<Aluno> listarTodosAlunos()
+    {
+        String sql = "SELECT * FROM aluno ORDER BY nome ASC";
+        List<Aluno> alunos = new ArrayList<>();
+
+
+
+        try (Connection conn = slqConn.getConnection()) {
+            PreparedStatement stnt = conn.prepareStatement(sql);
+            ResultSet rs = stnt.executeQuery();
+
+
+            while(rs.next()){
+                alunos.add(new Aluno(
+                        rs.getString("idAluno"),
+                        rs.getString("nome"),
+                        rs.getString("cpf"),
+                        rs.getString("email"),
+                        rs.getDate("data_nascimento").toLocalDate(),
+                        rs.getString("telefone")
+
+                ));
+            }
+
+        }catch (SQLException e){
+            System.out.println("Erro ao listar os alunos:" + e.getMessage());
+        }
+
+        return alunos;
     }
 
     @Override
--- untitled/src/main/java/org/example/App.java
+++ untitled/src/main/java/org/example/App.java
@@ -1,10 +1,71 @@
 package org.example;
 
+import daolmplements.AlunoDAOImplements;
 import database.slqConn;
-public class App 
+import model.Aluno;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
         slqConn.testeConnction();
-    }
-}
+
+        AlunoDAOImplements alunoDaoMethods = new AlunoDAOImplements();
+        Scanner sc = new Scanner(System.in);
+
+        int opcao;
+
+        do{
+
+            System.out.println("=======MENU=======");
+            System.out.println("1. Cadastrar Aluno");
+            System.out.println("2. Atualizar Aluno");
+            System.out.println("3. Excluir Aluno");
+            System.out.println("4 Listar Aluno");
+            System.out.println("5. Salvar Aluno");
+            System.out.println("0. Sair do programa");
+
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Cadrastro de aluno");
+                    break;
+                case 2:
+                    System.out.println("Atualizar aluno");
+                    break;
+                case 3:
+                    System.out.println("Excluir Aluno");
+                    break;
+                case 4:
+                    System.out.println("Listar Alunos");
+                    break;
+                case 5:
+                    System.out.println("Salvar Aluno");
+
+
+
+                List<Aluno> todosAlunos = alunoDaoMethods.listarTodosAlunos();
+
+                if (todosAlunos.isEmpty())
+                {
+                    System.out.println("Nenhum aluno encontrado.");
+                } else {
+                    for (Aluno aluno : todosAlunos){
+                        System.out.println(aluno);
+                    }
+                }
+            }
+
+        }while (opcao !=0);
+
+
+            }
+        }
+
+
+
